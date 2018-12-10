f0975182068427db02a25f6f77f9df67294b6290
azusa@westclan.jp <root@pegasus.west.home>
2018-12-10 23:24:51 +0900

    debug

--- game/answer.php
+++ game/answer.php
@@ -50,26 +50,27 @@ array_multisort($sortkey, SORT_ASC, $selorg);
 		echo "<td>".$selorg[$i][1]."</td>\n";
 	}
 	echo "</tr>\n";
-	for($i = 0; $i < $per-1; $i++){
+	for($i = 0; $i < $per; $i++){
 		$point[$i] = 0;
+		$count[$i] = 0;
+	}
+	for($i = 0; $i < $per-1; $i++){
 		echo "<tr>\n";
 		echo "<td>子".($i+1)."</td>";
 		for($j = 0; $j < $per; $j++){
 			if($j == $selans[$i]){
 				echo "<td>〇</td>\n";
-				if($i == $j){
-					$point[$i]--;
-				}else{
-					$count[$i+1]++;
+				if($i+1 == $j){
+					$point[$j]--;
+					echo "子".$j."さん自爆!減点1<br>";
 				}
 				if($j == 0){
-					$point[$i]++;
+					$point[$i+1]++;
+					echo "子".($i+1)."さん、正解!加点1<br>";
 				}
+				$count[$j]++;
 			}else{
 				echo "<td>&nbsp;</td>\n";
-				if($j == 0){
-					$count[0]++;
-				}
 			}
 		}
 		echo "</tr>\n";
@@ -80,31 +81,33 @@ array_multisort($sortkey, SORT_ASC, $selorg);
 	$minw = 0;
 	for($i = 0; $i < $per-1; $i++){
 		if($min > $count[$i]){
-			$min = $count;
+			$min = $count[$i];
 			$minw = $i;
 		}
 		if($max < $count[$i]){
-			$max = $count;
+			$max = $count[$i];
 			$maxw++;
 		}
 	}
 	if($minw == 0){
 		$point[0] = 2;
+		echo "親さん、だまし成功!加点2<br>";
 	}
-var_dump($max, $maxw, $count);
-	for($i = 0; $i < $per-1; $i++){
+	$poi = floor(2/$maxw*10)/10;
+	for($i = 1; $i < $per; $i++){
 		if($max == $count[$i]){
-			$point[$i+1] += 2/$maxw;
+			$point[$i] += $poi;
+			echo "子".$i."さん、最大".$max."人だまし成功!加点".$poi."<br>";
 		}
 	}
 ?>
 	</table><br>
-	Tenshuu<br>
+	点数<br>
 	<table border=1>
-	<tr><td>oya</td>
+	<tr><td>親</td>
 <?php
 	for($i = 0; $i < $per-1; $i++){
-		echo "<td>Ko".($i+1)."</td>";
+		echo "<td>子".($i+1)."</td>";
 	}
 	echo "</tr><tr>\n";
 	for($i = 0; $i < $per; $i++){
